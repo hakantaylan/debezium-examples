cd936a7f29f5957c5bd5311a7aa45c339fa74085
hakantaylan <hakantaylan@users.noreply.github.com>
2022-08-22 14:07:19 +0300

    new examples added

---
+++ spring-outbox-example-postgre-mongo-kafka/newsletter-service/src/main/java/com/eresh/outbox/mongo/model/NewsLetterSubscription.java
@@ -0,0 +1,23 @@
+package com.eresh.outbox.mongo.model;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+
+@Document(collection = "newsLetterSubscription")
+@Data
+@Builder
+public class NewsLetterSubscription {
+
+	@Id
+	private String id;
+	private String userId;
+	private String newsLetter;
+	private String frequency;
+	private String email;
+	private LocalDateTime createdOn;
+}
---
+++ spring-outbox-example-postgre-mongo-kafka/newsletter-service/src/main/java/com/eresh/outbox/mongo/repository/NewsLetterSubscriptionRepository.java
@@ -0,0 +1,13 @@
+package com.eresh.outbox.mongo.repository;
+
+import com.eresh.outbox.mongo.model.NewsLetterSubscription;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NewsLetterSubscriptionRepository extends MongoRepository<NewsLetterSubscription, String> {
+	List<NewsLetterSubscription> findByUserId(String userId);
+}
